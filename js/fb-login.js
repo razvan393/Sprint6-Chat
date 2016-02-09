803ae323d16f9402a58d3bd0fec701b14b3729f0
Roxana Ungureanu <ungureanu.roxi@yahoo.com>
2016-02-09 22:23:10 +0200

    fixed a bug of the login button

--- js/fb-login.js
+++ js/fb-login.js
@@ -13,13 +13,13 @@ function statusChangeCallback(response) {
     } else if (response.status === 'not_authorized') {
         // The person is logged into Facebook, but not your app.
         document.getElementById('status').innerHTML = 'Please log ' +
-            'into this app.';
-    } else {
-        // The person is not logged into Facebook, so we're not sure if
-        // they are logged into this app or not.
-        document.getElementById('status').innerHTML = 'Please log ' +
-            'into Facebook.';
-    }
+            'into this app.';}
+    //} else {
+    //    // The person is not logged into Facebook, so we're not sure if
+    //    // they are logged into this app or not.
+    //    document.getElementById('status').innerHTML = 'Please log ' +
+    //        'into Facebook.';
+    //}
 }
 
 function checkLoginState() {
