6d4c8ae7d4eca703f1d3d24e967ea192a4eab986
Roxana Ungureanu <ungureanu.roxi@yahoo.com>
2016-02-10 12:41:58 +0200

    fixed issues and added create participant functionality

--- js/main.js
+++ js/main.js
@@ -41,6 +41,7 @@
 
     app.controller('ParticipantsCtrl', function ($scope) {
         $scope.participants = user;
+
     });
 
     app.controller('FormCtrl', function ($scope) {
@@ -61,6 +62,7 @@
             $scope.reset();
         };
     });
+
 
     //create services
     app.factory('ParticipantsStore', function ($http, $q) {
@@ -156,7 +158,7 @@
                 getMessages: getMessages,
                 addMessage: addMessage
             };
-        })();
+        });
     });
 //    ------------------FACEBOOK API INIT------------------//
     window.fbAsyncInit = function() {
