77cf0144b9e14a9878260579fc832406097e4652
razvan393 <razvanghinea05@gmail.com>
2016-02-12 11:10:23 +0200

    Code fixes

--- js/fb-login.js
+++ js/fb-login.js
@@ -65,3 +65,23 @@
 //
 //    });
 //}
+//    ------------------FACEBOOK API INIT------------------//
+window.fbAsyncInit = function () {
+    FB.init({
+        appId: '1533557483610625',
+        xfbml: true,
+        version: 'v2.5'
+    });
+};
+
+(function (d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {
+        return;
+    }
+    js = d.createElement(s);
+    js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+    console.log('FB init');
+}(document, 'script', 'facebook-jssdk'));
